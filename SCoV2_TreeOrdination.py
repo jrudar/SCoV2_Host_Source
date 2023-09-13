3c8baa4e68bc6398b9aacbc0a8b4e33d1c2c60d5
jrudar <37217429+jrudar@users.noreply.github.com>
2023-09-13 10:06:25 -0400

    Add files via upload

--- SCoV2_TreeOrdination.py
+++ SCoV2_TreeOrdination.py
@@ -541,7 +541,7 @@ if __name__ == "__main__":
 
     prep_phylo = True
     if prep_phylo:
-        #Much of the following code was adapted from:
+        #Much of the following code was taken and adapted from:
         #Pickering, B., Lung, O., Maguire, F. et al. Divergent SARS-CoV-2 variant emerges in white-tailed deer with deer-to-human transmission. Nat Microbiol 7, 2011–2024 (2022). https://doi.org/10.1038/s41564-022-01268-9
         #This can be found here: https://github.com/fmaguire/on_deer_spillback_analyses/tree/master/analyses/maximum_likelihood_phylogenies
 
